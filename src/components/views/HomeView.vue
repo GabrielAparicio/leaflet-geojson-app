0579c6a3e6ab4e795b322995fb878a15d0ebd10c
Gabdev22 <gab.developer.22@gmail.com>
2020-03-10 23:21:18 -0500

    Adding home view for layout

---
+++ src/components/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="view-class">
+    <Home />
+  </div>
+</template>
+
+<script>
+import Home from "../Home.vue";
+export default {
+  name: "HomeView",
+  components: {
+    Home
+  }
+};
+</script>
