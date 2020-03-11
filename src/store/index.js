e82e92dfa67394a655f6e203c07a95adfadd213d
Gabdev22 <gab.developer.22@gmail.com>
2020-03-10 23:58:57 -0500

    Configuring vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,55 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import {
+  ADD_COORDINATE,
+  DELETE_COORDINATE,
+  UPDATE_COORDINATE
+} from "./mutationTypes";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    coordinates: [],
+    coordinateId: 1
+  },
+  getters: {
+    markers: state => state.coordinates
+  },
+  mutations: {
+    [ADD_COORDINATE](state, payload) {
+      state.coordinates.push({
+        id: state.coordinateId++,
+        position: payload.position,
+        radius: 10,
+        numOfSamples: 2
+      });
+    },
+    [DELETE_COORDINATE](state, payload) {
+      state.coordinates = state.coordinates.filter(
+        coordinate => coordinate.id != payload.id
+      );
+    },
+    [UPDATE_COORDINATE](state, payload) {
+      const { id, radius, numOfSamples } = payload;
+      state.coordinates = state.coordinates.map(coordinate => {
+        if (coordinate.id == id) {
+          return { ...coordinate, radius, numOfSamples };
+        }
+        return coordinate;
+      });
+    }
+  },
+  actions: {
+    [ADD_COORDINATE]({ commit }, payload) {
+      commit(ADD_COORDINATE, payload);
+    },
+    [DELETE_COORDINATE]({ commit }, payload) {
+      commit(DELETE_COORDINATE, payload);
+    },
+    [UPDATE_COORDINATE]({ commit }, payload) {
+      commit(UPDATE_COORDINATE, payload);
+    }
+  },
   modules: {}
 });
