b49add24f44eafa66ca9ce8fc8f55fed21b05894
Gabdev22 <gab.developer.22@gmail.com>
2020-03-11 00:20:30 -0500

    Including leaflet styles

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
+import "leaflet/dist/leaflet.css";
 import "./plugins/bootstrap-vue";
 
 Vue.config.productionTip = false;
