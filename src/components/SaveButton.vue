1e51628c0c2036d68c6c6e790837035a9e0aed26
Gabdev22 <gab.developer.22@gmail.com>
2020-03-10 23:47:57 -0500

    Create SaveButton component to show the list of markers

---
+++ src/components/SaveButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <b-button variant="info" @click="showData">Save</b-button>
+</template>
+
+<script>
+export default {
+  name: "SaveButton",
+  methods: {
+    showData() {
+      console.log("This button is goint to show the list of markers");
+    }
+  },
+  computed: {}
+};
+</script>
+
+<style></style>
